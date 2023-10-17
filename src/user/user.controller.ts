b616d5612f7c0ff670c6ca1bfa7fa10cb0b6175d
thien-le-corize <thien.le@corize.co.jp>
2023-10-17 22:36:45 +0700

    Feature decorator

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -10,6 +10,7 @@ import {
   UseInterceptors,
   ClassSerializerInterceptor,
   UseGuards,
+  Request,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/createUser.dto';
 import { UserService } from './user.service';
@@ -18,6 +19,8 @@ import { AuthGuard } from 'src/guards/auth.guard';
 import { RegisterUserDto } from './dtos/registerUser.dto';
 import { AuthService } from './auth.service';
 import { LoginUserDto } from './dtos/LoginUser.dto';
+import { CurrentUser } from './decorators/currentUser.decorator';
+import { UserEntity } from './entitites/user.entity';
 
 @Controller('user')
 @UseInterceptors(ClassSerializerInterceptor)
@@ -59,5 +62,11 @@ export class UserController {
   @Post('login')
   loginUser(@Body() requestBody: LoginUserDto) {
     return this.authService.login(requestBody);
+  }
+
+  @Get('/me')
+  @UseGuards(AuthGuard)
+  getCurrentUser(@CurrentUser() currentUser: UserEntity) {
+    return currentUser;
   }
 }
